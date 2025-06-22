5470f55563da12bfd0b9a22634b316bb420f198a
@telkomdev-eno <sutrisnoeno53@gmail.com>
2025-06-22 21:43:34 +0700

    feat: Setup Spatie

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,28 +1,21 @@
-window._ = require('lodash');
+import _ from 'lodash';
+import axios from 'axios';
 
-/**
- * We'll load the axios HTTP library which allows us to easily issue requests
- * to our Laravel back-end. This library automatically handles sending the
- * CSRF token as a header based on the value of the "XSRF" token cookie.
- */
+// Pasang ke window supaya bisa dipakai global di browser
+window._ = _;
+window.axios = axios;
 
-window.axios = require('axios');
-
+// Set header default axios
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
-/**
- * Echo exposes an expressive API for subscribing to channels and listening
- * for events that are broadcast by Laravel. Echo and event broadcasting
- * allows your team to easily build robust real-time web applications.
- */
-
+// Jika mau pakai Laravel Echo dan Pusher, contoh:
 // import Echo from 'laravel-echo';
+// import Pusher from 'pusher-js';
 
-// window.Pusher = require('pusher-js');
-
+// window.Pusher = Pusher;
 // window.Echo = new Echo({
-//     broadcaster: 'pusher',
-//     key: process.env.MIX_PUSHER_APP_KEY,
-//     cluster: process.env.MIX_PUSHER_APP_CLUSTER,
-//     forceTLS: true
+//   broadcaster: 'pusher',
+//   key: import.meta.env.VITE_PUSHER_APP_KEY, // atau process.env.MIX_PUSHER_APP_KEY tergantung config
+//   cluster: import.meta.env.VITE_PUSHER_APP_CLUSTER,
+//   forceTLS: true,
 // });
